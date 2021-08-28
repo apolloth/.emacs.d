2d56ec20e0f749bd9ff65c17fd263817233ce8a6
DeaserxX <deaser.lp@gmx.de>
2021-08-28 20:11:04 +0200

    Add new dashboard banners

--- configuration/base--startup.el
+++ configuration/base--startup.el
@@ -4,7 +4,7 @@
   :diminish page-break-lines-mode
   :config
   (setq
-   dashboard-startup-banner "~/.emacs.d/configuration/dashboard/emacs-logo.txt" 
+   dashboard-startup-banner "~/.emacs.d/configuration/dashboard/emacs-mascot.png"
    dashboard-items
    '((recents . 10)
      (projects . 5)))
