93200812e6224bcae9ec602028b7ac28ac3825c0
apolloth <deaser.lp@gmx.de>
2024-08-19 22:59:12 +0200

    Add QoL improvements for zshell integration

--- configuration/base--shell.el
+++ configuration/base--shell.el
@@ -1,135 +1,25 @@
-(use-package eshell
-  :ensure nil
-  :commands (eshell)
+(require 'tiling)
 
-  :init
-  (defvar eshell-mode-map* (make-sparse-keymap))
 
-  :bind*
-  (("M-! s s" . eshell-dwim)
-   ("M-! s S" . eshell)
-   ("M-! s c" . shell-command)
+(defun my-toggle-zshell ()
+  "Toggle a zshell buffer in a new window.
+   If the zsh buffer is visible in the current frame, bury it and delete
+   the window.  Otherwise, create a new window and switch to the zsh
+   buffer."
+  (interactive)
+  (let ((shell-buffer-name "*zshell*"))
+    (if (get-buffer-window shell-buffer-name)
+        (progn
+          (bury-buffer shell-buffer-name)
+          (delete-window (get-buffer-window shell-buffer-name)))
+      (let ((shell-buffer (get-buffer shell-buffer-name)))
+        (if shell-buffer
+            (pop-to-buffer shell-buffer)
+          (progn
+            (create-tiling-window)
+            (other-window 1)
+            (zshell)))))))
 
-   :map eshell-mode-map*
-   ("<up>" . previous-line)
-   ("<down>" . next-line)
-   ("C-<up>" . eshell-previous-prompt)
-   ("C-<down>" . eshell-next-prompt)
-
-   ("C-<return>" . end-of-buffer)
-
-   ("C-o" . eshell-insert-buffer-name)
-
-   ("M-p" . eshell-previous-matching-input-from-input)
-   ("M-n" . eshell-next-matching-input-from-input)
-
-   ("M-P" . counsel-esh-history))
-
-  :config
-  (require 'em-smart)
-
-  (defun eshell-dwim (arg)
-    (interactive "P")
-    (if (projectile-project-root)
-        (projectile-run-eshell arg)
-      (eshell arg)))
-
-  ;; OHA, weil eshell seine mode map nicht korrekt erstellt, und diese dadurch erst später zur Verfügung steht.
-  (add-hook 'eshell-mode-hook
-            (lambda ()
-              (replace-keymap-bindings eshell-mode-map* eshell-mode-map)))
-
-  (setq eshell-where-to-jump 'begin
-        eshell-review-quick-commands nil
-        eshell-smart-space-goes-to-end t
-        eshell-hist-ignoredups t
-        eshell-history-size 1024)
-
-  (setq eshell-rc-script "~/.eshellrc")
-
-  (setq eshell-prompt-function
-        (lambda ()
-          (let* ((home
-                  (getenv "HOME"))
-
-                 (pwd
-                  (replace-regexp-in-string "~" home (concat (eshell/pwd) "/")))
-
-                 (remote?
-                  (string-match tramp-file-name-regexp pwd))
-
-                 (local?
-                  (not remote?))
-
-                 (home-based-path
-                  (replace-regexp-in-string home "~" pwd))
-
-                 (project?
-                  (when (and local? (fboundp 'projectile-project-root))
-                    (projectile-project-root)))
-
-                 (project-root
-                  project?)
-
-                 (project-name
-                  (when project?
-                    (funcall projectile-project-name-function project-root)))
-
-                 (project-based-path
-                  (when project?
-                    (replace-regexp-in-string project-root ":/" pwd)))
-
-                 (git?
-                  (when (and project? (fboundp 'magit-get-current-branch))
-                    (magit-get-current-branch)))
-
-                 (git-branch
-                  (when git?
-                    (replace-regexp-in-string "master" "m" (magit-get-current-branch))))
-
-                 (git-modified?
-                  (and git? (magit-anything-modified-p)))
-
-                 (git-info
-                  (if git?
-                      (if git-modified?
-                          (propertize (format "(%s)" git-branch) 'face 'bold)
-                        (format "(%s)" git-branch))
-                    ""))
-
-                 (project-info
-                  (if project?
-                      (format "%s%s\n%s" project-name git-info project-based-path)
-                    ""))
-
-                 (info
-                  (if project?
-                      project-info
-                    home-based-path)))
-
-            (format "\n%s\n\u21b3 \u03bb " info)))
-
-        eshell-prompt-regexp
-        "^[^\u03bb]*\u03bb\s")
-
-  (defun eshell/repl (&rest profile)
-    (if (seq-empty-p profile)
-        (call-interactively 'cider-jack-in-dwim)
-      (cider-jack-in-with-profile (apply 'concat profile))))
-
-  (defun eshell/nvm (version)
-    (nvm-use version))
-
-  (add-hook
-   'eshell-mode-hook
-   '(lambda ()
-      (define-key eshell-mode-map (kbd "<tab>") 'completion-at-point))))
-
-(use-package eshell-up
-  :commands
-  (eshell-up eshell-up-peek))
-
-(use-package esh-autosuggest
-  :hook (eshell-mode . esh-autosuggest-mode))
+(global-set-key (kbd "C-<return>") 'my-toggle-zshell)
 
 (provide 'base--shell)
