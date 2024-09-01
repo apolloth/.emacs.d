9e4d71dcfe5e451738e1fe94eebcae9a8832b652
apolloth <deaser.lp@gmx.de>
2024-09-01 19:23:14 +0200

    Add org-agenda to dashboard and change spacing of dashboard items

--- configuration/base--startup.el
+++ configuration/base--startup.el
@@ -5,11 +5,32 @@
 
   :custom
   ((dashboard-startup-banner "~/.emacs.d/configuration/dashboard/emacs-mascot.png")
-   (dashboard-items '((recents . 10)
-                      (projects . 5)
-                      (agenda . 5))))
+   (dashboard-items '((recents  .  10)
+                      (projects .   5)
+                      (agenda   .   5)))
+   (dashboard-icon-type 'nerd-icons)
+   (dashboard-set-file-icons t)
+   (dashboard-set-heading-icons t)
+   (dashboard-center-content t)
+   (dashboard-filter-agenda-entry 'dashboard-filter-agenda-by-todo)
+
+   (dashboard-startupify-list
+    '(dashboard-insert-banner
+      dashboard-insert-banner-title
+      dashboard-insert-newline
+      dashboard-insert-newline
+      dashboard-insert-items
+      dashboard-insert-newline
+      dashboard-insert-newline
+      dashboard-insert-newline
+      dashboard-insert-newline
+      dashboard-insert-newline
+      dashboard-insert-newline
+      dashboard-insert-newline
+      dashboard-insert-footer)))
 
   :config
   (dashboard-setup-startup-hook))
+
 
 (provide 'base--startup)
